4cb250015aeca9b8afd6d7c481a10fbf8c571b5a
Sebastian Jensen <apps@tsjdev-apps.de>
2022-04-09 18:40:35 +0200

    Add needed converters for account page.

---
+++ WordPressXF/WordPressXF/WordPressXF/Converters/NameToInitialsConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WordPressXF.Converters
+{
+    internal class NameToInitialsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string name)
+                return string.Empty;
+
+            if (string.IsNullOrEmpty(name))
+                return string.Empty;
+
+            var words = name.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length == 1)
+                return words[0][0].ToString();
+
+            if (words.Length > 1)
+                return words[0][0].ToString() + words[words.Length - 1][0].ToString();
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WordPressXF/WordPressXF/WordPressXF/Converters/NullToIsVisibleConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WordPressXF.Converters
+{
+    internal class NullToIsVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return value == null;
+
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
