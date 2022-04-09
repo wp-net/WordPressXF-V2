5cc48d7ef350d3bf93c6d871497f21d8bc72826c
Sebastian Jensen <apps@tsjdev-apps.de>
2022-04-09 12:29:30 +0200

    Update Statics file.

--- WordPressXF/WordPressXF/WordPressXF/Common/Statics.cs
+++ WordPressXF/WordPressXF/WordPressXF/Common/Statics.cs
@@ -3,5 +3,10 @@
     internal static class Statics
     {
         public static string WordpressUrl = "https://developer.wordpress.org/wp-json";
+
+        public static int PageSize = 10;
+
+        public static string UsernameSettingsKey = "UsernameSettings";
+        public static string PasswordSettingsKey = "PasswordSettings";
     }
 }
