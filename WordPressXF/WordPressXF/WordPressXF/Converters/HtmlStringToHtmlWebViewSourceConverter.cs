3296d6f15749968f3526409448695da0e97042c2
Sebastian Jensen <apps@tsjdev-apps.de>
2022-04-09 17:31:16 +0200

    Add converter needed for showing the detail post.

---
+++ WordPressXF/WordPressXF/WordPressXF/Converters/HtmlStringToHtmlWebViewSourceConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using WordPressPCL.Models;
+using WordPressXF.Common;
+using Xamarin.Forms;
+
+namespace WordPressXF.Converters
+{
+    internal class HtmlStringToHtmlWebViewSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Post post)
+            {
+                var htmlContent = HtmlTools.CreateHtmlFromPost(post);
+                return new HtmlWebViewSource { Html = htmlContent };
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
