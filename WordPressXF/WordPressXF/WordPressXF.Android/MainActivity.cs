4b1058ec60f2378cd88a710d3af021ff96b22377
Sebastian Jensen <apps@tsjdev-apps.de>
2022-04-09 18:54:15 +0200

    Add app icon for Android.

--- WordPressXF/WordPressXF/WordPressXF.Android/MainActivity.cs
+++ WordPressXF/WordPressXF/WordPressXF.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 
 namespace WordPressXF.Droid
 {
-    [Activity(Label = "WordPressXF", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "WordPressXF", Icon = "@mipmap/appicon", RoundIcon = "@mipmap/appiconround", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
